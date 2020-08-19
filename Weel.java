add9a97dd0934c934c612b51e090608a8ec0979f
khadak-bogati <61652776+khadak-bogati@users.noreply.github.com>
2020-08-18 21:12:39 -0500

    Import Scanner system

---
+++ Weel.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Weel {
+  public static void main(String[] args){
+    System.out.println("Plase provide a real.");
+    Scanner s = new Scanner(System.in);
+    double d = s.nextDouble();
+    
+    System.out.println(d);
+  }
+}
